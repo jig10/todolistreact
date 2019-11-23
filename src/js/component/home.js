2143da3ee2e59fd8adcff8f71656fe378d80f515
jig10 <jigodoyrubin@gmail.com>
2019-11-23 16:40:48 +0000

    second commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [todos, setTodos] = useState([
+		<li key={1}>hacer camaa</li>,
+		<li key={2}>hacer tarea</li>,
+		<li key={3}>hacer ejercio</li>,
+		<li key={4}>ducharme</li>
+	]);
+    const [tempInputVal, setTemporalInputVal] = useState()
+	//todos.push(<li key={5}>comer</li>);
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input onChange={(e) => setTemporalInputVal (e.target.value)}/>
+			<button
+				onClick={() => setTodos(todos.concat(<li key={5}>{tempInputVal}</li>))}>
+				Add
+			</button>
+            
+			<ul>{todos}</ul>
 		</div>
 	);
 }
